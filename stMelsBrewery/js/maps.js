bb83c59872b53b73e53145d3f92aa156f7ea9aa9
uselessMonkeys <paulconway993@gmail.com>
2016-11-10 20:20:02 +0000

    [stMelsBrewery] Contact Us layout changed

---
+++ stMelsBrewery/js/maps.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru = {lat: 53.734406, lng: -7.782982};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
