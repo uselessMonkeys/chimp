c737f4561f4e2870e6c35f9c0ab4880659e55aa0
uselessMonkeys <paulconway993@gmail.com>
2016-11-13 15:30:23 +0000

    [stMelsBrewery] Contact Us -Google maps completed

--- stMelsBrewery/js/maps.js
+++ stMelsBrewery/js/maps.js
@@ -1,11 +1,19 @@
+/*
+    Goggle Maps Api
+    API KEY = AIzaSyD3h7mZcHdepqeGNHMunyyASdf3B6_-YNQ
+*/
 function initMap() {
-        var uluru = {lat: 53.734406, lng: -7.782982};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 16,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
+    var uluru = {lat: 53.734406, lng: -7.782982};
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+    });
+    
+    var marker = new google.maps.Marker({
+        position: uluru,
+        icon: 'css/img/stMelsBreweryLogoGoogleMaps.png',
+        title: 'St. Mels Brewey',
+        map: map
+    });
+}
